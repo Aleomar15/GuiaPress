94df16c15ac7316f91a60b62423a2757cd24a82e
Alexandre O Marinho <alexandreomar0202@gmail.com>
2023-04-04 16:53:47 -0300

    Cadastrando as categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,9 +1,21 @@
 const express =  require("express");
 const router = express.Router();//Cria rotas sem estar no seu arquivo principal
-router.get("/categories",(req,res) =>{
-    res.send("ROTA DE CATEGORIAS")
-});
+const Category = require("./Category");
+const slugify = require("slugify");
 router.get("/admin/categories/new", (req,res)=>{
     res.render("admin/categories/new" );
+});
+router.post("/categories/save",(req,res)=>{
+    var title = req.body.title;
+    if(title != undefined){
+        Category.create({//irá salvar os dados no bd
+            title: title,
+            slug: slugify(title)
+        }).then(()=>{
+            res.redirect("/");
+        });
+    }else{
+        res.redirect("/admin/categories/new");
+    }
 });
 module.exports = router;
