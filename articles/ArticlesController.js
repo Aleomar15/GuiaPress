d586b52987036e9a96ba4cd885195b11c98c3547
Alexandre O Marinho <alexandreomar0202@gmail.com>
2023-05-19 16:23:35 -0300

    Criendo e deletando artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,9 +1,50 @@
 const express =  require("express");
 const router = express.Router();//Cria rotas sem estar no seu arquivo principal
-router.get("/articles",(req,res) =>{
-    res.send("ROTA DE ARTIGOA")
+const Category = require("../categories/Category")
+const Article = require("./Article");
+const slugify = require("slugify");
+router.get("/admin/articles",(req,res) =>{
+   Article.findAll({
+    include: [{model: Category}]
+   }).then(articles =>{
+    res.render("admin/articles/index",{articles: articles})
+   })
 });
 router.get("/admin/articles/new", (req,res)=>{
-    res.render("admin/articles/new")
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new",{categories: categories})
+    })    
+});
+router.post("/articles/save", (req,res)=>{
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(()=>{
+        res.redirect("/admin/articles");
+    })
+});
+router.post("/articles/delete", (req, res)=>{
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){//verifica se o id é numero
+            Article.destroy({//irá deletar a categoria pelo id
+                where:{
+                    id: id
+                }
+            }).then(()=>{
+                res.redirect("/admin/articles");
+            });
+        }else{//se não for um número
+            res.redirect("/admin/articles");
+        }
+    }else{// se  for nulo
+        res.redirect("/admin/articles");
+    }
 });
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -1,6 +1,7 @@
 const  Sequelize  = require("sequelize");
 const connection = new Sequelize('guiapress','root','',{
     host: 'localhost',
-    dialect:'mysql'
+    dialect:'mysql',
+    timezone:"-03:00" // timezone do Brasil
 });
 module.exports = connection;
