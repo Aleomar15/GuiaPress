2946b290b994db6124284a2dc2c373be6792fbf0
Alexandre O Marinho <alexandreomar0202@gmail.com>
2023-05-30 16:29:55 -0300

    Editando artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -47,4 +47,36 @@ router.post("/articles/delete", (req, res)=>{
         res.redirect("/admin/articles");
     }
 });
+
+router.get("/admin/articles/edit/:id", (req, res)=>{
+    var id = req.params.id;
+    Article.findByPk(id).then(article =>{
+    if(article != undefined){
+        Category.findAll().then(categories =>{
+            res.render("admin/articles/edit", {categories: categories, article: article})
+        });
+    }else{
+        res.redirect("/");
+    }
+    }).catch(err =>{
+        res.redirect("/");
+    });
+});
+
+router.post("/articles/update", (req, res)=>{
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.update({title: title, body: body, categoryId: category, slug:slugify(title)},{
+        where: {
+            id: id
+        }
+    }).then(()=>{
+        res.redirect("/admin/articles");
+    }).catch(err =>{
+        res.redirect("/");
+    });
+});
 module.exports = router;
