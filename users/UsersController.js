0ce3f2ae47344631921552d55b4f483a859591b8
Alexandre O Marinho <alexandreomar0202@gmail.com>
2023-06-13 16:13:28 -0300

    Criando o usuario ecripitando senha

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./Users");
+const bcrypt = require("bcryptjs");// biblioteca de cryptografia
 
 router.get("/admin/users", (req, res)=>{
-    res.send("Listagem de usuarios");
+    User.findAll().then(users =>{
+        res.render("admin/users/index",{users: users});
+    });
 })
 router.get("/admin/users/create", (req, res)=>{
     res.render("admin/users/create");
@@ -11,6 +14,24 @@ router.get("/admin/users/create", (req, res)=>{
 router.post("/users/create", (req,res)=>{
     var email = req.body.email;
     var password = req.body.password;
+
+    User.findOne({where:{email: email}}).then(user=>{
+        if(user == undefined){
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(password, salt);
+
+            User.create({
+                email: email,
+                password: hash
+            }).then(()=>{
+                res.redirect("/");
+            }).catch((err)=>{
+                res.redirect("/");
+            });
+        }else{
+            res.redirect("/admin/users/create");
+        }
+    });
 
     //res.json({email,password}) para ver se os dados estão retornando
 });
